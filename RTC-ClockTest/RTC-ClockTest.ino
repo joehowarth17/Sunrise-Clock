0d2f0a3774a6f6178abfd33e6cdfc8da6ec533f8
Joseph Howarth <josephhowarth17@gmail.com>
2017-05-11 15:30:46 -0500

    add everything

---
+++ RTC-ClockTest/RTC-ClockTest.ino
@@ -0,0 +1,23 @@
+#include <Wire.h>
+byte* a;
+//byte* b;
+void setup() {
+  // put your setup code here, to run once:
+Wire.begin(); 
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+Wire.beginTransmission(0x03);
+Wire.write(0);
+Wire.endTransmission();
+Wire.requestFrom(0x03,1);
+*a = Wire.read();
+//*b = Wire.read();
+//byte b = Wire.read();
+//Serial.print(*b,BIN);
+Serial.println(*a,BIN);
+
+delay(2000);
+}
