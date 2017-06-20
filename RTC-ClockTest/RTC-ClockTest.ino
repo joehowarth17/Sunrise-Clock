efdc1d9b97b288e9ffe8780a04df83d4b4256f6c
Joseph Howarth <josephhowarth17@gmail.com>
2017-06-20 03:03:50 -0500

    Added Esp8266 Neopixel program

--- RTC-ClockTest/RTC-ClockTest.ino
+++ RTC-ClockTest/RTC-ClockTest.ino
@@ -1,23 +1,27 @@
 #include <Wire.h>
-byte* a;
-//byte* b;
+const int PCF8523_ADDRESS =byte( 0x68);
+
 void setup() {
   // put your setup code here, to run once:
 Wire.begin(); 
 Serial.begin(9600);
 }
-
+int reading =0;
 void loop() {
   // put your main code here, to run repeatedly:
-Wire.beginTransmission(0x03);
-Wire.write(0);
-Wire.endTransmission();
-Wire.requestFrom(0x03,1);
-*a = Wire.read();
-//*b = Wire.read();
-//byte b = Wire.read();
-//Serial.print(*b,BIN);
-Serial.println(*a,BIN);
 
-delay(2000);
+Wire.beginTransmission(PCF8523_ADDRESS);
+Wire.write(byte(0x00));
+Wire.endTransmission();
+
+Wire.requestFrom(PCF8523_ADDRESS,3);
+if (3 <= Wire.available()) { // if two bytes were received
+    reading = Wire.read();  // receive high byte (overwrites previous reading)
+    reading = reading << 16;    // shift high byte to be high 8 bits
+    reading |= Wire.read()<<8; // receive low byte as lower 8 bits
+    reading |= Wire.read();
+    Serial.println(reading);   // print the reading
+  }
+
+delay(1000);
 }
