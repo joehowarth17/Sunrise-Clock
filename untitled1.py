f3f2764061c817d3f96047916d157152e9c3a1b7
joehowarth17 <josephhowarth17@gmail.com>
2018-01-06 22:02:47 -0600

    Removed serial log files.

--- untitled1.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  3 17:10:07 2017
-
-@author: Joseph
-"""
-numbers = {0:'ABCDEF',1:'BC',2:'ABGED',3:'ABGCD',4:'FGCB',5:'AFGCD',6:'ACDEFG'
-           ,7:'ABC',8:'ABCDEFG',9:'ABCFG'}
-pins
